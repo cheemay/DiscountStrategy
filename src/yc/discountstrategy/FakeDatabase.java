8c083f1fa1d923953ce4cf10a1f5e2c9151be348
ycheema <ycheema@L11703042766.wctc.msft>
2016-02-23 15:25:16 -0600

    DiscountStrategy-FirstCommit

---
+++ src/yc/discountstrategy/FakeDatabase.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yc.discountstrategy;
+
+/**
+ *
+ * @author ycheema
+ */
+public class FakeDatabase {
+    //private Customer [] customers = new Customer [3];
+    
+    private Customer [] customers = {
+        new Customer("100" , "Bob James"),
+        new Customer("200" , "Sally Smith"),
+        new Customer("300" , "Peter Piper"),
+    
+    };
+    private Product [] producsts = new Product [3];
+    
+}
